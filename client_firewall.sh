3329b8f10a2567992cce4b8fe85818dd698ddef5
Darkerego <darkerego@users.noreply.github.com>
2015-07-14 00:27:50 -0400

    Update client_firewall.sh
    Added echo points for debugging and fixed some errors.

--- client_firewall.sh
+++ client_firewall.sh
@@ -17,21 +17,24 @@ logDNSC='invalid dnscrypt'
 logTMS='invalid transmission'
 logTUN='invalid vpn sever!'
 # Variables
-vpnPRT=65432
-vpnSSH=33567
+vpnPRT=51194
+vpnSSH=33222
 vpnSVR=1.2.3.4
-vpnCLI=10.9.0.1
+vpnCLI=10.9.0.4
 tunSVR=10.9.0.1
 DNS_1=208.67.222.222
 DNS_2=208.67.220.220
 transPRT=51431
 domain=53
 http=80
-service1="" # some other ports
-service2=""
-localSN='192.168.1.1/24'
+transWEB=9777
+localSN='192.168.253.0/24'
+localDNS=192.168.253.1
+#service2=some_other_port
 #And interface (usually tun0 or tap0)
-vpnITF=tun+
+vpnITF=tun0
+wfITF=wlan0
+$ethIF=eth0
 #Flush our current chains:
 #
 $IPT -F
@@ -45,9 +48,9 @@ ufw default deny incoming
 ufw default deny outgoing
 echo "Defaults set..."
 ## DNS Queries should pass to initiate the connection
-##ufw allow out 53 to 192.168.1.1
-##ufw allow out to 208.67.222.222 port 443 proto udp
-##ufw allow out to 208.67.220.220 port 443 proto udp
+$IPT -A OUTPUT -i $ethIF -p tcp --dport 53 -d $localDNS -j ACCEPT
+$IPT -A OUTPUT -i $ethIF -p udp --dport 53 -d $localDNS -j ACCEPT
+
 ##Only allow out to server on vpn port
 if [ -f '/root/scripts/blocked.ips' ];
 then
@@ -68,28 +71,28 @@ tPP_EN=false
 if [[ $tPP_EN == "true" ]]; then
 echo Warning: Transmission peer port open...
 ####
-$IPT -A INPUT -i tun+ -p tcp --dport $transPRT -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
-$IPT -A INPUT -i tun+ -p udp --dport $transPRT -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
+$IPT -A INPUT -i $vpnITF -p tcp --dport $transPRT -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
+$IPT -A INPUT -i $vpnITF -p udp --dport $transPRT -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
 else
 echo Not openening transmission peer port...
 fi
 echo Setting vpn rules...
 ####Allow VPN
-$IPT -A OUTPUT -o tun+ -j ACCEPT
-$IPT -A OUTPUT -o eth0 -p tcp -d $vpnSVR --dport $vpnPRT -j ACCEPT
-$IPT -A OUTPUT -o eth0 -p udp -d $vpnSVR --dport $vpnPRT -j ACCEPT
-$IPT -A OUTPUT -o wlan0 -p tcp -d $vpnSVR --dport $vpnPRT -j ACCEPT
-$IPT -A OUTPUT -o wlan0 -p udp -d $vpnSVR --dport $vpnPRT -j ACCEPT
-$IPT -A OUTPUT -o eth0 -p tcp -d $vpnSVR --dport $vpnSSH -j ACCEPT
-$IPT -A OUTPUT -o wlan0 -p tcp -d $vpnSVR --dport $vpnSSH -j ACCEPT
+$IPT -A OUTPUT -o $vpnITF -j ACCEPT
+$IPT -A OUTPUT -o $ethIF -p tcp -d $vpnSVR --dport $vpnPRT -j ACCEPT
+$IPT -A OUTPUT -o $ethIF -p udp -d $vpnSVR --dport $vpnPRT -j ACCEPT
+$IPT -A OUTPUT -o $wfITF -p tcp -d $vpnSVR --dport $vpnPRT -j ACCEPT
+$IPT -A OUTPUT -o $wfITF -p udp -d $vpnSVR --dport $vpnPRT -j ACCEPT
+$IPT -A OUTPUT -o $ethIF -p tcp -d $vpnSVR --dport $vpnSSH -j ACCEPT
+$IPT -A OUTPUT -o $wfITF -p tcp -d $vpnSVR --dport $vpnSSH -j ACCEPT
 echo Preventing transmission leaks...
 ## Prevent Transmission Leaks
-$IPT -A OUTPUT -o tun+ -p tcp --dport $transPRT -j ACCEPT
-$IPT -A OUTPUT -o tun+ -p udp --dport $transPRT -j ACCEPT
-$IPT -A OUTPUT -o eth0 -p tcp --dport $transPRT -j REJECT
-$IPT -A OUTPUT -o eth0 -p udp --dport $transPRT -j REJECT
-$IPT -A OUTPUT -o wlan+ -p tcp --dport $transPRT -j REJECT
-$IPT -A OUTPUT -o wlan+ -p udp --dport $transPRT -j REJECT
+$IPT -A OUTPUT -o $vpnITF -p tcp --dport $transPRT -j ACCEPT
+$IPT -A OUTPUT -o $vpnITF -p udp --dport $transPRT -j ACCEPT
+$IPT -A OUTPUT -o $ethIF -p tcp --dport $transPRT -j REJECT
+$IPT -A OUTPUT -o $ethIF -p udp --dport $transPRT -j REJECT
+$IPT -A OUTPUT -o $wfITF -p tcp --dport $transPRT -j REJECT
+$IPT -A OUTPUT -o $wfITF -p udp --dport $transPRT -j REJECT
 echo Setting dnscrypt rules...
 # Dnscrypt-proxy rules
 $IPT -A OUTPUT -p udp -m owner --uid-owner dnscrypt -d $DNS_1 --sport 1024:65535 --dport 443 -j ACCEPT
@@ -98,19 +101,19 @@ $IPT -A OUTPUT -m owner --uid-owner dnscrypt -j LOG --log-prefix '$logDNSC'
 $IPT -A OUTPUT -m owner --uid-owner dnscrypt -j REJECT
 echo Setting vpn-reachable services....
 # VPN Services we need to reach
-$IPT -A OUTPUT -o tun+ -s $vpnCLI -d $tunSVR -p udp --dport $domain -j ACCEPT
-$IPT -A OUTPUT -o tun+ -s $vpnCLI -d $tunSVR -p tcp --dport $http -j ACCEPT
-$IPT -A OUTPUT -o tun+ -s $vpnCLI -d $tunSVR -p tcp --dport $service -j ACCEPT
-$IPT -A OUTPUT -o tun+ -s $vpnCLI -d $tunSVR -p tcp --dport $service2 -j ACCEPT
-$IPT -A OUTPUT -o tun+ -s $vpnCLI -d $tunSVR -p tcp --dport $vpnSSH -j ACCEPT
-$IPT -A OUTPUT -o tun+ -d $tunSVR -j LOG --log-prefix='$logTUN'
-$IPT -A OUTPUT -o tun+ -d $tunSVR -j REJECT
+$IPT -A OUTPUT -o $vpnITF -s $vpnCLI -d $tunSVR -p udp --dport $domain -j ACCEPT
+$IPT -A OUTPUT -o $vpnITF -s $vpnCLI -d $tunSVR -p tcp --dport $http -j ACCEPT
+$IPT -A OUTPUT -o $vpnITF -s $vpnCLI -d $tunSVR -p tcp --dport $transWEB -j ACCEPT
+#$IPT -A OUTPUT -o $vpnITF -s $vpnCLI -d $tunSVR -p tcp --dport $service2 -j ACCEPT
+$IPT -A OUTPUT -o $vpnITF -s $vpnCLI -d $tunSVR -p tcp --dport $vpnSSH -j ACCEPT
+$IPT -A OUTPUT -o $vpnITF -d $tunSVR -j LOG --log-prefix='$logTUN'
+$IPT -A OUTPUT -o $vpnITF -d $tunSVR -j REJECT
 echo Setting private network settings...
 ##Allow access to all private networks.
-$IPT -A OUTPUT -o tun+ -d $localSN -j REJECT
-$IPT -A OUTPUT -o tun+ -d $localSN -j REJECT
-$IPT -A OUTPUT -o wlan+ -d $localSN -j ACCEPT
-$IPT -A OUTPUT -o eth0 -d $localSN -j ACCEPT
+$IPT -A OUTPUT -o $vpnITF -d $localSN -j REJECT
+$IPT -A OUTPUT -o $vpnITF -d $localSN -j REJECT
+$IPT -A OUTPUT -o $wfITF -d $localSN -j ACCEPT
+$IPT -A OUTPUT -o $ethIF -d $localSN -j ACCEPT
 echo Will now enable the fw...
 ## Or let UFW handle it if you're less paranoid
 #ufw allow out to 192.168.0.0/16
